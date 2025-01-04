e0ac7a41d793d3150d2d1caa71c7a0d5011c9d38
SohailAli2021 <sohail.qaazi@gmail.com>
2025-01-04 17:38:43 +0500

    first commit

---
+++ WMS/MDI.Designer.cs
@@ -0,0 +1,176 @@
+﻿namespace WMS
+{
+    partial class MDI
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtjoborderbarcode = new System.Windows.Forms.TextBox();
+            this.txtweight = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(18, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(154, 19);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Job Order Barcode";
+            // 
+            // txtjoborderbarcode
+            // 
+            this.txtjoborderbarcode.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtjoborderbarcode.Location = new System.Drawing.Point(22, 31);
+            this.txtjoborderbarcode.Name = "txtjoborderbarcode";
+            this.txtjoborderbarcode.Size = new System.Drawing.Size(150, 20);
+            this.txtjoborderbarcode.TabIndex = 3;
+            // 
+            // txtweight
+            // 
+            this.txtweight.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtweight.Location = new System.Drawing.Point(292, 31);
+            this.txtweight.Name = "txtweight";
+            this.txtweight.Size = new System.Drawing.Size(91, 20);
+            this.txtweight.TabIndex = 4;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToResizeColumns = false;
+            this.dataGridView1.AllowUserToResizeRows = false;
+            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3,
+            this.Column4});
+            this.dataGridView1.Location = new System.Drawing.Point(22, 57);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(618, 521);
+            this.dataGridView1.TabIndex = 7;
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(288, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(62, 19);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Weight";
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "Job Order ID";
+            this.Column1.Name = "Column1";
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "Job Order Date";
+            this.Column2.Name = "Column2";
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "Product Name";
+            this.Column3.Name = "Column3";
+            // 
+            // Column4
+            // 
+            this.Column4.HeaderText = "Customer Name";
+            this.Column4.Name = "Column4";
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(521, 17);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(119, 34);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Print Weight Label";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // MDI
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.ClientSize = new System.Drawing.Size(698, 590);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.txtweight);
+            this.Controls.Add(this.txtjoborderbarcode);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.ForeColor = System.Drawing.SystemColors.InfoText;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow;
+            this.Name = "MDI";
+            this.Text = "MDI";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtjoborderbarcode;
+        private System.Windows.Forms.TextBox txtweight;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ WMS/MDI.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Data;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace WMS
+{
+    public partial class MDI : Form
+    {
+        private BindingSource bindingSource;
+        private DataTable barcodeTable;
+
+        public MDI()
+        {
+            InitializeComponent();
+
+            // Initialize the DataTable to store barcode data
+            barcodeTable = new DataTable();
+            barcodeTable.Columns.Add("Scanned Barcode");
+            barcodeTable.Columns.Add("Live Weight");
+
+            // Initialize the BindingSource with the DataTable
+            bindingSource = new BindingSource();
+            bindingSource.DataSource = barcodeTable;
+
+            // Bind the DataGridView to the BindingSource
+            dataGridView1.DataSource = bindingSource;
+
+            // Ensure KeyPress event is connected
+            txtjoborderbarcode.KeyPress += txtBarcode_KeyPress;
+            txtweight.KeyPress += txtweight_KeyPress;
+            // Optionally, set the column width to make sure it's visible
+            dataGridView1.Columns[0].Width = 100;
+            dataGridView1.Columns[1].Width = 100;
+
+            // Adjust as necessary
+        }  // To check barcode input
+
+        private void txtBarcode_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            // If the Enter key is pressed, process the scanned barcode
+            if (e.KeyChar == (char)Keys.Enter)
+            {
+                string scannedBarcode = txtjoborderbarcode.Text.Trim();
+
+                // Only add the barcode if it's not empty
+                if (!string.IsNullOrEmpty(scannedBarcode))
+                {
+                    // Add the barcode to the DataTable (which will update the BindingSource)
+                    barcodeTable.Rows.Add(scannedBarcode);
+
+                    // Clear the TextBox for the next scan
+                    txtjoborderbarcode.Clear();
+
+                    // Optionally, refresh the DataGridView
+                    dataGridView1.Refresh();
+                }
+
+
+
+
+            }
+        }
+
+        private void txtweight_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            // If the Enter key is pressed, process the weight input
+            if (e.KeyChar == (char)Keys.Enter)
+            {
+                string weight = txtweight.Text.Trim();
+
+                // Only add the weight if it's not empty
+                if (!string.IsNullOrEmpty(weight))
+                {
+                    // Get the last row added to ensure we associate the weight with the correct barcode
+                    if (barcodeTable.Rows.Count > 0)
+                    {
+                        // Update the "Live Weight" column for the last added row
+                        barcodeTable.Rows[barcodeTable.Rows.Count - 1]["Live Weight"] = weight;
+
+                        // Clear the TextBox for the next weight input
+                        txtweight.Clear();
+
+                        // Optionally, refresh the DataGridView
+                        dataGridView1.Refresh();
+                    }
+                    else
+                    {
+                        MessageBox.Show("Please scan a barcode before entering the weight.", "Input Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    }
+                }
+
+                Console.WriteLine("Weight: " + weight);
+            }
+        }
+
+    }
+}
